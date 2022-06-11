67735a35456ad4559599aba96bd8f0c594e8086f
Niki <nikit@LAPTOP-3RKDFSVS.(none)>
2022-06-11 14:18:17 +0530

    Changes committed

--- app.js
+++ app.js
@@ -3,20 +3,17 @@ const ejs=require("ejs");
 const express=require("express");
 const mongoose=require('mongoose');
 
-const app=express(); //new app instance
+const app=express(); 
 app.set('view engine','ejs');
 
-app.use(bodyParser.urlencoded({extended:true})); //b-p to pass our requests
+app.use(bodyParser.urlencoded({extended:true})); 
 
-app.use(express.static("public")); //to store static files such as images,css code
+mongoose.connect("mongodb://localhost:27017/dolistDB"); 
 
-mongoose.connect("mongodb://localhost:27017/dolistDB"); //Allows mongoose to connect to local mongoDB instance
 
-///////////////////////////////////////////////USERSCHEMA
 const userSchema={
     username: {
         type: String,
-        unique: true,
         required: true,
       },
       password: {
@@ -25,82 +22,100 @@ const userSchema={
       },
     email:{
         type: String,
-        unique: true,
         required: true,
     },
+    title:String,
+    entrystatus:String
+
 
 };
 
-// const User=mongoose.model("User",userSchema);
+const User=mongoose.model("User",userSchema);
 
-// app.post("/register",function(req,res){
-    
-//     const newUser=new User{{
-//         username: request.body.username,
-//         // email:req.body.email,
-//         // password:req.body.password
-//     }}
-
-// })
-
-
-
-
-////////////////////////////////////////////////////////
-const doEntriesSchema={
-    title:String
-};
-
-const Entry=mongoose.model("Entry",doEntriesSchema);
-
-app.route("/entries")
-.get(function(req, res){
-    Entry.find(function(err, foundEntry){
-      if(!err){
-      res.send(foundEntry);
-      }else{
-          res.send(err);
-      }
-    });
-  })
-  .post(function(req,res){
-    console.log(req.body.title);
-
-    const newEntry=new Entry({
-        title:req.body.title
-    });
-
-    newEntry.save(function(err){
+app.route("/users")
+.get(function(req,res){
+    User.find(function(err,foundUser){
         if(!err){
-            res.send("Successfully added an entry.")
+            res.send(foundUser);
+            
         }else{
             res.send(err);
         }
     });
 })
-.put(function(req,res){
-    Entry.updateOne(
-        {title:req.body.title,},
-        {overwrite:true},
-        function(err){
-            if(!err){
-                res.send("Updated entry");
-            }
-        }        
-    )
+.post(function(req,res){
 
+    const newUser=new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        title:req.body.title,
+        entrystatus:req.body.entrystatus
+
+    });
+    newUser.save(function(err){
+        if(!err){
+            res.send("Successfully added a user entry.")
+        }else{
+            res.send(err);
+        }
+    });
 })
 
+
 .delete(function(req,res){
-    Entry.deleteMany(function(err){
+    User.deleteMany(
+        function(err){
         if(!err){
-            res.send("Successfully deleted all entries");
+            res.send("Successfully deleted entry");
         }else{
             res.send(err);
         }
     });
 });
 
+app.route("/users/:title")
+.get(function(req,res){
+    User.findOne({title:req.params.title},function(err,foundEntry){
+        if(foundEntry){
+            res.send(foundEntry);
+        }else{
+        res.send("No title matching");
+        }
+    });
+})
+
+
+.patch(function(req, res){
+
+    User.updateOne(
+      {title: req.params.title},
+      {$set: req.body},
+      function(err){
+        if(!err){
+          res.send("updated entry.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  })
+  
+  .delete(function(req, res){
+  
+    User.deleteOne(
+      {title: req.params.title},
+      function(err){
+        if (!err){
+          res.send("Successfully deleted entry.");
+        } else {
+          res.send(err);
+        }
+      }
+    );
+  });
+  
+  
 
 
 app.listen(3000,function(){
